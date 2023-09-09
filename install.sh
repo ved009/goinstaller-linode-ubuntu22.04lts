e34ad743cd063bcccf41569687dc2bf582e607b6
Vedant Roy <vedantroy42@gmail.com>
2023-09-09 21:08:17 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ sha256sum go1.21.1.linux-amd64.tar.gz;
 sudo tar -C /usr/local -xvf go1.21.1.linux-amd64.tar.gz;
 echo -e '#Go PATH\nexport PATH=$PATH:/usr/local/go/bin' >> ~/.profile;
 source ~/.profile;
+go version;
 echo "Go is installed"
