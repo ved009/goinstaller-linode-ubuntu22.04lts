d91a44d921970ad1f14fb0b6ccfa51e0906c911b
Vedant Roy <vedantroy42@gmail.com>
2023-09-09 20:27:21 +0530

    Update install.sh
    corrected echo write

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ apt get update $$ apt get upgrade;
 curl -OL https://golang.org/dl/go1.21.1.linux-amd64.tar.gz;
 sha256sum go1.21.1.linux-amd64.tar.gz;
 sudo tar -C /usr/local -xvf go1.21.1.linux-amd64.tar.gz;
-echo -e "#Go PATH\n export PATH=$PATH:/usr/local/go/bin" << ~/.profile;
+echo -e '#Go PATH\nexport PATH=$PATH:/usr/local/go/bin' >> ~/.profile;
 source ~/.profile;
 echo "Go is installed"
