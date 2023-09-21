ff82b0b51a61fb8c76c1a1499d2e1802572f62e9
Vedant Roy <vedantroy42@gmail.com>
2023-09-21 17:56:19 +0530

    Update install.sh
    for root Linodes

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ apt get update $$ apt get upgrade;
 curl -OL https://golang.org/dl/go1.21.1.linux-amd64.tar.gz;
 sha256sum go1.21.1.linux-amd64.tar.gz;
 sudo tar -C /usr/local -xvf go1.21.1.linux-amd64.tar.gz;
-echo -e '#Go PATH\nexport PATH=$PATH:/usr/local/go/bin' >> ~/.profile;
+echo -e '#Go PATH\nexport PATH=$PATH:/root/go/bin' >> ~/.profile;
 source ~/.profile;
 go version;
 echo "Go is installed"
